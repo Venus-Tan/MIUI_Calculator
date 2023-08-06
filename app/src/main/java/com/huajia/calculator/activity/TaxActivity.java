c4947bd3c104381653fddcc814a47f4006459f99
tanhuajia <821759439@qq.com>
2023-08-06 12:35:12 +0800

    v1.0.4 UI基本完成        路飞觉醒五档太阳神尼卡形态，好像有点无厘头但又有点自由的感觉        自由，究竟要怎么定义呢        2023.08.06

--- app/src/main/java/com/huajia/calculator/activity/TaxActivity.java
+++ app/src/main/java/com/huajia/calculator/activity/TaxActivity.java
@@ -1,16 +1,28 @@
 package com.huajia.calculator.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.huajia.calculator.R;
 
-public class TaxActivity extends AppCompatActivity {
+public class TaxActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tax);
+        findViewById(R.id.returnImg).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.returnImg:
+                finish();
+                break;
+        }
     }
 }
